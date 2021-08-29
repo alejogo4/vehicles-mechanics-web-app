61442f7a5d0ac63faa367accaf8c2527dfbd74ac
alejogo4 <alejo.go4@hotmail.com>
2021-08-29 12:26:37 -0500

    Crud document types and brands

---
+++ VehiclesAPI/Migrations/20210829164528_RunProceduresBrandAndDocumentType.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VehiclesAPI.Migrations
+{
+    public partial class RunProceduresBrandAndDocumentType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
